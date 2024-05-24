e8077a94caad93008e7f0f7d5f11a3bea3147811
hrytsykvl <gricikvlad@gmail.com>
2024-05-24 10:30:52 +0300

    Add project files.

---
+++ ProgModule3/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+/*
+ Зчитує дані з файлу employees.xml. Файл містить список співробітників у форматі XML, де кожен співробітник має такі властивості: Name, Position, HireDate.
+Сортує співробітників за датою прийому на роботу (від найстаріших до найновіших) за допомогою LINQ.
+Зберігає відсортований список співробітників у новий XML файл sorted_employees.xml.
+Записує інформацію про співробітників в текстовий файл employees.txt у наступному форматі:
+
+Name: [Name] Position: [Position] HireDate: [HireDate]
+*/
+
+public class Employee
+{
+	public string Name { get; set; }
+	public string Position { get; set; }
+	public DateTime HireDate { get; set; }
+
+	public override string ToString()
+	{
+		return $"Name: {Name}, Position: {Position}, HireDate: {HireDate}";
+	}
+}
+
+public class Program
+{
+	public static void Main()
+	{
+		string inputPath = "E:\\Projects C-type lang\\C# Projects\\ProgModule3\\ProgModule3\\employees.xml";
+		string filteredPath = "E:\\Projects C-type lang\\C# Projects\\ProgModule3\\ProgModule3\\sorted_employees.xml";
+		//string updatedOutputFilePath = "UpdatedLibrary.xml";
+
+		List<Employee> employees = DeserializeEmployees(inputPath);
+		employees.ForEach(Console.WriteLine);
+
+		//var filteredBooks = employees.Where(book => book.Year > 2015)
+		//						 .OrderByDescending(book => book.Price)
+		//						 .ToList();
+
+		//SerializeEmployees(filteredBooks, filteredPath);
+
+		//employees.RemoveAll(book => book.Author == "John Doe");
+
+		//SerializeEmployees(employees, updatedOutputFilePath);
+	}
+
+	private static List<Employee> DeserializeEmployees(string filePath)
+	{
+		XmlSerializer serializer = new XmlSerializer(typeof(List<Employee>), new XmlRootAttribute("Employees"));
+		using (FileStream fs = new FileStream(filePath, FileMode.Open))
+		{
+			return (List<Employee>)serializer.Deserialize(fs);
+		}
+	}
+
+	private static void SerializeEmployees(List<Employee> employees, string filePath)
+	{
+		XmlSerializer serializer = new XmlSerializer(typeof(List<Employee>), new XmlRootAttribute("Employees"));
+		using (StreamWriter writer = new StreamWriter(filePath))
+		{
+			serializer.Serialize(writer, employees);
+		}
+	}
+}
